83b45cbcd575678d4a20fb96cc77f095fc55cfb1
Jeruko <jeruizko10@gmail.com>
2025-12-03 14:16:10 +0100

    Minion & Tile display

--- Scripts/Game/Display/MinionDisplay.cs
+++ Scripts/Game/Display/MinionDisplay.cs
@@ -1,8 +1,36 @@
+using Components;
 using Godot;
+using Utility;
 
 namespace Game;
 
 public partial class MinionDisplay : Node2D
 {
+	[ExportSubgroup("Squash Animation")]
+	[Export] public float AnimationSpeed { get; set; } = 0.7f;
 
+	[ExportSubgroup("References")]
+	[Export] public RootState RootState { get; private set; }
+	[Export] public Sprite2D Sprite { get; private set; }
+	[Export] Sprite2D colorOverlay;
+	[Export] public SquashStretch2D SquashAnimator { get; private set; }
+	[Export] public ColorOverlayModule FlashEffect { get; private set; }
+	[Export] public OutlineModule OutlineModule { get; private set; }
+
+	float modifier, elapsedTime;
+
+    public override void _Ready()
+	{
+		SquashAnimator.SetSquashModulator(() => modifier);
+    }
+
+	public override void _Process(double delta)
+	{
+		colorOverlay.Texture = Sprite.Texture;
+		colorOverlay.Transform = Sprite.Transform;
+
+		elapsedTime += (float)delta;
+		elapsedTime = Logic.LoopRange(elapsedTime, 0, 1000);
+		modifier = Mathf.Sin(elapsedTime * Mathf.Pi * 2f * AnimationSpeed);
+	}
 }
--- Scripts/Game/Display/TileDisplay.cs
+++ Scripts/Game/Display/TileDisplay.cs
@@ -5,5 +5,7 @@ namespace Game;
 
 public partial class TileDisplay : Node2D
 {
-
+	[ExportSubgroup("References")]
+	[Export] public Sprite2D Sprite { get; private set; }
+	
 }
